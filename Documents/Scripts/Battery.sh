5b41f0e0d298df7015720ba806cd497e0ec42350
Dheer <dheersoni13@gmail.com>
2025-09-25 16:01:53 +0530

    Updated

--- Documents/Scripts/Battery.sh
+++ Documents/Scripts/Battery.sh
@@ -4,22 +4,20 @@ BAT="/sys/class/power_supply/BAT0"
 CAPACITY=$(cat "$BAT/capacity")
 STATUS=$(cat "$BAT/status")
 
-# Choose icon based on percentage
 if [ "$CAPACITY" -ge 80 ]; then
-  ICON=""
+  ICON="󰂀"
 elif [ "$CAPACITY" -ge 60 ]; then
-  ICON=""
+  ICON="󰁿"
 elif [ "$CAPACITY" -ge 40 ]; then
-  ICON=""
+  ICON="󰁽"
 elif [ "$CAPACITY" -ge 20 ]; then
-  ICON=""
+  ICON="󰁻"
 else
-  ICON=""
+  ICON="󰁺"
 fi
 
-# Add charging icon overlay if charging
 if [ "$STATUS" = "Charging" ]; then
-  ICON=" $ICON" # lightning bolt + battery
+  ICON=""
 fi
 
 echo "$ICON $CAPACITY%"
